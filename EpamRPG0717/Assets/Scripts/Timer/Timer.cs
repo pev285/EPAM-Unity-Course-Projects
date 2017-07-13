d676b54fd636c32c8e764305b5728b417a82bb8c
pev-VAIO\pev <prosolupov@inbox.ru>
2017-07-13 05:03:07 +0300

    Второй коммит

---
+++ EpamRPG0717/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer  {
+
+    TimerComponent tc;
+
+    public Timer(GameObject go, UnityAction action, float time, int repetitions)
+    {
+        tc = go.AddComponent<TimerComponent>();
+
+        tc.Action = action;
+        tc.TimeToWait = time;
+        tc.RepetitionsNumber = repetitions;
+    }
+
+
+    public void StopTimer()
+    {
+        tc.TimeToWait = 100000;
+        tc.RepetitionsNumber = 0;
+    }
+
+}
---
+++ EpamRPG0717/Assets/Scripts/Timer/TimerComponent.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerComponent : MonoBehaviour {
+
+    private UnityAction action;
+    private float timeSpan;
+    private float timeVar;
+    private int repetitionsNumber;
+
+    public int RepetitionsNumber
+    {
+        get
+        {
+            return RepetitionsNumber;
+        }
+        set
+        {
+            RepetitionsNumber = value;
+        }
+    }
+
+
+    public UnityAction Action
+    {
+        get
+        {
+            return action;
+        }
+
+        set
+        {
+            action = value;
+        }
+    }
+
+    public float TimeToWait
+    {
+        get
+        {
+            return timeSpan;
+        }
+
+        set
+        {
+            timeVar = value;
+            timeSpan = value;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (repetitionsNumber == 0)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            timeVar -= Time.deltaTime;
+
+            if (timeVar <= 0)
+            {
+                action();
+                repetitionsNumber--;
+                timeVar = timeSpan;
+            }
+        }
+
+	}
+}
