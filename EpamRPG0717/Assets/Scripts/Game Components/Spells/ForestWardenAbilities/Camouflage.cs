e4e42d600a26d578207eb1d2c23808b3c4260223
pev-VAIO\pev <prosolupov@inbox.ru>
2017-07-13 17:44:45 +0300

    save before the class

---
+++ EpamRPG0717/Assets/Scripts/Game Components/Spells/ForestWardenAbilities/Camouflage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camouflage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
